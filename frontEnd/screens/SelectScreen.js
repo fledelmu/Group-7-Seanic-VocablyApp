26dc0f33fecec8fa4fbec80e5df60b67c8cdba81
Allain <xdallain@gmail.com>
2024-11-06 17:38:41 +0800

    first commit

--- frontEnd/screens/SelectScreen.js
+++ frontEnd/screens/SelectScreen.js
@@ -13,7 +13,7 @@ function HomeScreen({ navigation }) {
             <View key={letter} style={styles.letterContainer}>
               <Text
                 style={styles.letter}
-                onPress={() => navigation.navigate('AlphabetTestScreen', { letter })}
+                onPress={() => navigation.navigate('AlphabetViewScreen', { letter })}
               >
                 {letter}
               </Text>
